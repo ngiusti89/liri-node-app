1382b6442473c1500b0bf121896046d92c89fcb0
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-05-04 20:06:24 -0700

    function calls working

--- liri.js
+++ liri.js
@@ -5,23 +5,43 @@ var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 var query = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
-function concertThis () {
 
+// function telling app which function to run based on user input
+function execute (query) {
+    if (query === "concert-this") {
+        concertThis(input);
+      }
+      if (query === "spotify-this-song") {
+        spotifyThis(input);
+      }
+      if (query === "movie-this") {
+        movieThis(input);
+      }
+      if (query === "do-what-it-says") {
+        doWhatItSays();
+      }
+}
+
+function concertThis (input) {
+    console.log(input);
 }
 
 function spotifyThis () {
-
+    console.log(input)
 }
 
 function movieThis () {
-
+    console.log(input)
 }
 
 function doWhatItSays () {
-    
+    console.log(input)
 }
+
+console.log(query);
+execute(query);
