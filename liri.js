840adab4dda2a7adc31391d8f34ebb9a31c506de
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-05-04 21:28:08 -0700

    working spotify search anddisplay

--- liri.js
+++ liri.js
@@ -4,8 +4,8 @@ const keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 var query = process.argv[2];
 var input = process.argv.slice(3).join(" ");
@@ -30,18 +30,38 @@ function execute(query) {
 function concertThis(input) {
     console.log(input);
     axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(function (response) {
-        
+
         response.data.forEach(concert => {
+            console.log("\n")
             console.log("Venue: " + concert.venue.name)
-            console.log("Location: " +concert.venue.city + ", " + concert.venue.region)
+            console.log("Location: " + concert.venue.city + ", " + concert.venue.region)
             console.log("Date: " + moment(concert.datetime).format("MM/DD/YYYY"))
             console.log("\n")
         })
     })
 }
 
-function spotifyThis() {
+function spotifyThis(input) {
     console.log(input)
+    // if no user input, will search for this song
+    if (!input) {
+        input = "The Sign Ace of Base";
+    }
+    spotify.search({
+        type: 'track',
+        query: input
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("\n")
+        console.log("Artist: " + data.tracks.items[0].artists[0].name)
+        console.log("Song: " + data.tracks.items[0].name)
+        console.log("Preview: " + data.tracks.items[3].preview_url)
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("\n")
+
+    });
 }
 
 function movieThis() {
